a648078b575c4ed9b2c437443306718227b1a86c
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2024-07-16 23:51:50 +0600

    Generate stubs for Fluent Form 1.17.2

--- forminator-constants-stubs.php
+++ forminator-constants-stubs.php
@@ -95,8 +95,8 @@
  * Author URI: http://wpmudev.com
  */
 \define('FORMINATOR_ADDON_ZAPIER_VERSION', '1.2');
-\define('FORMINATOR_VERSION', '1.17.1');
-\define('FORMINATOR_SUI_VERSION', '2.11.2');
+\define('FORMINATOR_VERSION', '1.17.2');
+\define('FORMINATOR_SUI_VERSION', '2.12.10');
 \define('FORMINATOR_STRIPE_LIB_VERSION', '7.67.0');
 \define('FORMINATOR_STRIPE_LIB_DATE', '2019-12-03');
 \define('FORMINATOR_STRIPE_PARTNER_ID', 'pp_partner_GeDaq2odDgGkDJ');
