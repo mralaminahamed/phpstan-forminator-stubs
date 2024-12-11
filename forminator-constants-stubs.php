b7318354ff999b50756780599d2006ae16caf2bf
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2024-12-11 21:34:16 +0600

    Generate stubs for Fluent Form 1.36.2

--- forminator-constants-stubs.php
+++ forminator-constants-stubs.php
@@ -5,173 +5,160 @@
  * @see https://github.com/mralaminahamed/phpstan-forminator-stubs
  */
 
-/*
- * This file is part of the Symfony package.
+/**
+ * The Activecampaign.
  *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * @package    Forminator
  */
-namespace ForminatorGoogleAddon\Symfony\Polyfill\Intl\Grapheme {
-    \define('SYMFONY_GRAPHEME_CLUSTER_RX', ((float) \PCRE_VERSION < 10 ? (float) \PCRE_VERSION >= 8.32 : (float) \PCRE_VERSION >= 10.39) ? '\\X' : \ForminatorGoogleAddon\Symfony\Polyfill\Intl\Grapheme\Grapheme::GRAPHEME_CLUSTER_RX);
-}
-namespace {
-    /**
-     * The Activecampaign.
-     *
-     * @package    Forminator
-     */
-    /**
-     * Integration Name: Activecampaign
-     * Version: 1.0
-     * Plugin URI:  https://wpmudev.com/
-     * Description: Integrate Forminator Custom Forms with Activecampaign to get notified in real time.
-     * Author: WPMU DEV
-     * Author URI: http://wpmudev.com
-     */
-    \define('FORMINATOR_ADDON_ACTIVECAMPAIGN_VERSION', '1.0');
-    /**
-     * Integration Name: Aweber
-     * Version: 1.0
-     * Plugin URI:  https://wpmudev.com/
-     * Description: Integrate Forminator Custom Forms with Aweber to get notified in real time.
-     * Author: WPMU DEV
-     * Author URI: http://wpmudev.com
-     *
-     * @package Forminator
-     */
-    \define('FORMINATOR_ADDON_AWEBER_VERSION', '1.0');
-    /**
-     * Integration Name: Campaignmonitor
-     * Version: 1.0
-     * Plugin URI:  https://wpmudev.com/
-     * Description: Integrate Forminator Custom Forms with Campaignmonitor to get notified in real time.
-     * Author: WPMU DEV
-     * Author URI: http://wpmudev.com
-     *
-     * @package Forminator
-     */
-    \define('FORMINATOR_ADDON_CAMPAIGNMONITOR_VERSION', '1.0');
-    /**
-     * Integration Name: Google Sheets
-     * Version: 1.1
-     * Plugin URI:  https://wpmudev.com/
-     * Description: Integrate Forminator Custom Forms and Polls with Google Sheets to get notified in real time.
-     * Author: WPMU DEV
-     * Author URI: http://wpmudev.com
-     *
-     * @package Forminator
-     */
-    \define('FORMINATOR_ADDON_GOOGLESHEET_VERSION', '2.16.0');
-    /**
-     * Integration Name: Gutenberg
-     * Version: 1.0
-     * Plugin URI:  https://wpmudev.com/
-     * Description: Gutenberg blocks for Forminator
-     * Author: WPMU DEV
-     * Author URI: http://wpmudev.com
-     *
-     * @package Forminator
-     */
-    \define('FORMINATOR_ADDON_GUTENBERG_VERSION', '1.0');
-    /**
-     * Integration Name: HubSpot
-     * Version: 1.0
-     * Plugin URI:  https://wpmudev.com/
-     * Description: Integrate Forminator Custom Forms with HubSpot to get notified in real time.
-     * Author: WPMU DEV
-     * Author URI: http://wpmudev.com
-     *
-     * @package Forminator
-     */
-    \define('FORMINATOR_ADDON_HUBSPOT_VERSION', '1.0');
-    /**
-     * Integration Name: Mailchimp
-     * Version: 1.0
-     * Plugin URI:  https://wpmudev.com/
-     * Description: Integrate Forminator Custom Forms with Mailchimp email list easily
-     * Author: WPMU DEV
-     * Author URI: http://wpmudev.com
-     *
-     * @package Forminator
-     */
-    \define('FORMINATOR_ADDON_MAILCHIMP_VERSION', '1.0');
-    /**
-     * Integration Name: MailerLite
-     * Version: 1.0
-     * Plugin URI:  https://wpmudev.com/
-     * Description: Integrate Forminator Custom Forms with MailerLite to get notified in real time.
-     * Author: WPMU DEV
-     * Author URI: http://wpmudev.com
-     *
-     * @package Forminator
-     */
-    \define('FORMINATOR_ADDON_MAILERLITE_VERSION', '1.0');
-    /**
-     * Integration Name: Mailjet
-     * Version: 1.0
-     * Plugin URI:  https://wpmudev.com/
-     * Description: Integrate Forminator Modules with Mailjet email list easily
-     * Author: WPMU DEV
-     * Author URI: http://wpmudev.com
-     *
-     * @package Forminator
-     */
-    \define('FORMINATOR_ADDON_MAILJET_VERSION', '1.0');
-    /**
-     * Integration Name: Slack
-     * Version: 1.0
-     * Plugin URI:  https://wpmudev.com/
-     * Description: Integrate Forminator Custom Forms with Slack to get notified in real time.
-     * Author: WPMU DEV
-     * Author URI: http://wpmudev.com
-     *
-     * @package Forminator
-     */
-    \define('FORMINATOR_ADDON_SLACK_VERSION', '1.1');
-    /**
-     * Integration Name: Trello
-     * Version: 1.0
-     * Plugin URI:  https://wpmudev.com/
-     * Description: Integrate Forminator Custom Forms with Trello to get notified in real time.
-     * Author: WPMU DEV
-     * Author URI: http://wpmudev.com
-     *
-     * @package Forminator
-     */
-    \define('FORMINATOR_ADDON_TRELLO_VERSION', '1.1');
-    /**
-     * Integration Name: Webhook
-     * Version: 1.0
-     * Plugin URI:  https://wpmudev.com/
-     * Description: Integrate Forminator Custom Forms with Webhook to execute various action you like
-     * Author: WPMU DEV
-     * Author URI: http://wpmudev.com
-     *
-     * @package Forminator
-     */
-    \define('FORMINATOR_ADDON_WEBHOOK_VERSION', '1.0');
-    \define('FORMINATOR_VERSION', '1.36.1');
-    \define('FORMINATOR_SUI_VERSION', '2.12.23');
-    \define('FORMINATOR_STRIPE_LIB_VERSION', '14.6.0');
-    \define('FORMINATOR_STRIPE_LIB_DATE', '2024-04-10');
-    \define('FORMINATOR_STRIPE_PARTNER_ID', 'pp_partner_GeDaq2odDgGkDJ');
-    \define('FORMINATOR_PAYPAL_LIB_VERSION', '1.14.0');
-    \define('FORMINATOR_PRO', \false);
-    \define('FORMINATOR_PRO_URL', 'https://wpmudev.com/project/forminator-pro/');
-    \define('FORMINATOR_PLUGIN_BASENAME', \plugin_basename(__FILE__));
-    /**
-     * Plugin Name: Forminator Simple Addon
-     * Version: 1
-     * Description: Simple Addon forminator.
-     * Author: WPMU DEV
-     * Author URI: http://wpmudev.com
-     * Text Domain: external_forminator
-     * Domain Path: /languages/
-     *
-     * @package Forminator
-     */
-    // Direct Load.
-    \define('FORMINATOR_ADDON_SIMPLE_VERSION', '1.0');
-}
+/**
+ * Integration Name: Activecampaign
+ * Version: 1.0
+ * Plugin URI:  https://wpmudev.com/
+ * Description: Integrate Forminator Custom Forms with Activecampaign to get notified in real time.
+ * Author: WPMU DEV
+ * Author URI: http://wpmudev.com
+ */
+\define('FORMINATOR_ADDON_ACTIVECAMPAIGN_VERSION', '1.0');
+/**
+ * Integration Name: Aweber
+ * Version: 1.0
+ * Plugin URI:  https://wpmudev.com/
+ * Description: Integrate Forminator Custom Forms with Aweber to get notified in real time.
+ * Author: WPMU DEV
+ * Author URI: http://wpmudev.com
+ *
+ * @package Forminator
+ */
+\define('FORMINATOR_ADDON_AWEBER_VERSION', '1.0');
+/**
+ * Integration Name: Campaignmonitor
+ * Version: 1.0
+ * Plugin URI:  https://wpmudev.com/
+ * Description: Integrate Forminator Custom Forms with Campaignmonitor to get notified in real time.
+ * Author: WPMU DEV
+ * Author URI: http://wpmudev.com
+ *
+ * @package Forminator
+ */
+\define('FORMINATOR_ADDON_CAMPAIGNMONITOR_VERSION', '1.0');
+/**
+ * Integration Name: Google Sheets
+ * Version: 1.1
+ * Plugin URI:  https://wpmudev.com/
+ * Description: Integrate Forminator Custom Forms and Polls with Google Sheets to get notified in real time.
+ * Author: WPMU DEV
+ * Author URI: http://wpmudev.com
+ *
+ * @package Forminator
+ */
+\define('FORMINATOR_ADDON_GOOGLESHEET_VERSION', '2.16.0');
+/**
+ * Integration Name: Gutenberg
+ * Version: 1.0
+ * Plugin URI:  https://wpmudev.com/
+ * Description: Gutenberg blocks for Forminator
+ * Author: WPMU DEV
+ * Author URI: http://wpmudev.com
+ *
+ * @package Forminator
+ */
+\define('FORMINATOR_ADDON_GUTENBERG_VERSION', '1.0');
+/**
+ * Integration Name: HubSpot
+ * Version: 1.0
+ * Plugin URI:  https://wpmudev.com/
+ * Description: Integrate Forminator Custom Forms with HubSpot to get notified in real time.
+ * Author: WPMU DEV
+ * Author URI: http://wpmudev.com
+ *
+ * @package Forminator
+ */
+\define('FORMINATOR_ADDON_HUBSPOT_VERSION', '1.0');
+/**
+ * Integration Name: Mailchimp
+ * Version: 1.0
+ * Plugin URI:  https://wpmudev.com/
+ * Description: Integrate Forminator Custom Forms with Mailchimp email list easily
+ * Author: WPMU DEV
+ * Author URI: http://wpmudev.com
+ *
+ * @package Forminator
+ */
+\define('FORMINATOR_ADDON_MAILCHIMP_VERSION', '1.0');
+/**
+ * Integration Name: MailerLite
+ * Version: 1.0
+ * Plugin URI:  https://wpmudev.com/
+ * Description: Integrate Forminator Custom Forms with MailerLite to get notified in real time.
+ * Author: WPMU DEV
+ * Author URI: http://wpmudev.com
+ *
+ * @package Forminator
+ */
+\define('FORMINATOR_ADDON_MAILERLITE_VERSION', '1.0');
+/**
+ * Integration Name: Mailjet
+ * Version: 1.0
+ * Plugin URI:  https://wpmudev.com/
+ * Description: Integrate Forminator Modules with Mailjet email list easily
+ * Author: WPMU DEV
+ * Author URI: http://wpmudev.com
+ *
+ * @package Forminator
+ */
+\define('FORMINATOR_ADDON_MAILJET_VERSION', '1.0');
+/**
+ * Integration Name: Slack
+ * Version: 1.0
+ * Plugin URI:  https://wpmudev.com/
+ * Description: Integrate Forminator Custom Forms with Slack to get notified in real time.
+ * Author: WPMU DEV
+ * Author URI: http://wpmudev.com
+ *
+ * @package Forminator
+ */
+\define('FORMINATOR_ADDON_SLACK_VERSION', '1.1');
+/**
+ * Integration Name: Trello
+ * Version: 1.0
+ * Plugin URI:  https://wpmudev.com/
+ * Description: Integrate Forminator Custom Forms with Trello to get notified in real time.
+ * Author: WPMU DEV
+ * Author URI: http://wpmudev.com
+ *
+ * @package Forminator
+ */
+\define('FORMINATOR_ADDON_TRELLO_VERSION', '1.1');
+/**
+ * Integration Name: Webhook
+ * Version: 1.0
+ * Plugin URI:  https://wpmudev.com/
+ * Description: Integrate Forminator Custom Forms with Webhook to execute various action you like
+ * Author: WPMU DEV
+ * Author URI: http://wpmudev.com
+ *
+ * @package Forminator
+ */
+\define('FORMINATOR_ADDON_WEBHOOK_VERSION', '1.0');
+\define('FORMINATOR_VERSION', '1.36.2');
+\define('FORMINATOR_SUI_VERSION', '2.12.23');
+\define('FORMINATOR_STRIPE_LIB_VERSION', '14.6.0');
+\define('FORMINATOR_STRIPE_LIB_DATE', '2024-04-10');
+\define('FORMINATOR_STRIPE_PARTNER_ID', 'pp_partner_GeDaq2odDgGkDJ');
+\define('FORMINATOR_PAYPAL_LIB_VERSION', '1.14.0');
+\define('FORMINATOR_PRO', \false);
+\define('FORMINATOR_PRO_URL', 'https://wpmudev.com/project/forminator-pro/');
+\define('FORMINATOR_PLUGIN_BASENAME', \plugin_basename(__FILE__));
+/**
+ * Plugin Name: Forminator Simple Addon
+ * Version: 1
+ * Description: Simple Addon forminator.
+ * Author: WPMU DEV
+ * Author URI: http://wpmudev.com
+ * Text Domain: external_forminator
+ * Domain Path: /languages/
+ *
+ * @package Forminator
+ */
+// Direct Load.
+\define('FORMINATOR_ADDON_SIMPLE_VERSION', '1.0');
